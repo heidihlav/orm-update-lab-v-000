1f5cb0b442613cf1530f79f4e4351ddc7ac278c9
Heidi Hlavinka <hchlav@gmail.com>
2018-10-15 18:42:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-
+    Song.new(result[0], result[1], result[2])
   end
 
   # Remember, you can access your database connection anywhere in this class
