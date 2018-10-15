b9cea6994e09acaba97d5f67e37c52f8e13d7942
Heidi Hlavinka <hchlav@gmail.com>
2018-10-15 18:32:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,10 +52,10 @@ class Student
 
   def self.new_from_db(row)
     # binding.pry
-    Student.new(id, name, grade)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
 
   end
 
